6c8bda77bd6cc79577877a272b7bc7f05c8cf435
BraedenKummer21 <813664@mystma.org>
2021-03-24 15:37:23 +0000

    topFiveMovies try 1

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 #WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a + b + c + d + e
+
+print(topFiveMovies("Pacific Rim, ", "Boy and the Beast, ", "A Silent Voice, ", "Real Steel, ", "No Game No Life Zero"))
