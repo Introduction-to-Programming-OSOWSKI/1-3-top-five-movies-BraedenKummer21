5fdd1c32516aa72cdbb98962c30387133e837f6a
BraedenKummer21 <813664@mystma.org>
2021-03-25 14:21:21 +0000

    topFiveMovies try 10

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 def topFiveMovies(a, b, c, d, e):
     return a + b + c + d + e
 
-print(topFiveMovies("Pacific Rim" " ", "Boy and the Beast" " ", "A Silent Voice" " ", "Real Steel" " ", "No Game No Life Zero"))
+print(topFiveMovies("Pacific Rim ", "Boy and the Beast ", "A Silent Voice ", "Real Steel ", "No Game No Life Zero"))
